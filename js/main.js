37b718fe93b138b44ad3a7f4ea714f9729f7b889
madebyrag <madebyrag@qq.com>
2015-08-01 12:07:03 +0800

    修复居中问题,加入导航条

--- js/main.js
+++ js/main.js
@@ -16,3 +16,28 @@ $(document).ready(function() {
         })
     })();
 });
+
+//为处理一个bug,暂不知道出现的原因.由于只有在调整页面才会出现该bug,加载时不会,所以放到最后
+/*window.addEventListener('resize', function(){
+	var html = document.getElementsByTagName('html')[0];
+	if(html.offsetWidth/html.offsetHeight < 16/9) {
+        var width = html.offsetHeight *16/9 +'px';
+        document.getElementById('hoverImgContainer').style.width = width;
+        document.getElementById('bgImg').style.width = width;
+    }
+	else {
+        document.getElementById('hoverImgContainer').style.width = '100%';
+        document.getElementById('bgImg').style.width = '100%';
+    }
+});*/
+$('#menu').bind('click', function() {
+    if($(this).attr('status') && $(this).attr('status') == 'unfolded') {
+        $(this).css('transform', 'rotate(0deg)');
+        $(this).attr('status', 'folded');
+        $('#nav').css('width', '32px');
+    } else {
+        $(this).css('transform', 'rotate(1800deg)');
+        $(this).attr('status', 'unfolded');
+        $('#nav').css('width', '500px');
+    }
+});
