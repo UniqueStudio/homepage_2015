83f1a2c0b0a67c01956229e32ae011378e1c9e80
madebyrag <madebyrag@qq.com>
2015-08-06 14:29:02 +0800

    使用yeoman将工程重构

---
+++ scripts/data.js
@@ -0,0 +1,64 @@
+define(function(require, exports, module) {
+	require('jquery');
+	var data = {};
+	//以防在没加载完就开始滚动
+	data.isActive = false;
+	data.changing = false;
+	data.block = {
+		'default': 0,
+		'group': 0,
+		'greatEvent': 0,
+		'works': 0
+	}
+	function scroll(e, param) {
+		//param中包括direction, quantity, target($), block
+		if(data.changing || data.isActive == false) return;
+		
+		var delta;
+		if(e.originalEvent.wheelDelta) {
+			if(e.originalEvent.wheelDelta > 0) {
+				delta = 100;
+			} else {
+				delta = -100;
+			}
+		} else if(e.originalEvent.detail) {
+			if(e.originalEvent.detail > 0) {
+				delta = -100;
+			} else {
+				delta = 100;
+			}
+		} else {
+			switch(e.which){
+				case ((param.direction == 'y')? 40:39) :
+					delta = -100;
+					break;
+				case ((param.direction == 'y')? 38:37) :
+					delta = 100;
+					break;
+				default:
+					//如果不这么做,按其他键会导致不触发,但设置了后面内容
+					return;
+			}
+		}
+
+		//判断是第一个或最后一个	
+		if(delta > 0 && data.block[param.block] == 0 || delta < 0 && data.block[param.block] == param.quantity) {
+			if(param.block == 'default') return;
+			param.target = $('#main');
+			param.block = 'default';
+			param.direction = 'y';
+		}
+
+		var topStr = param.target[0].style[((param.direction == 'y')? 'top':'left')].replace('%', '');
+		var topNum = Number(topStr) + delta;
+		param.target.css(((param.direction == 'y')? 'top':'left'), topNum + '%');
+		data.changing = true;
+		data.block[param.block] += (delta > 0)? -1:1;
+	}
+	data.scrollFunc = {
+		'default': function(e) {scroll(e, {direction: 'y', quantity: 5, target: $('#main'), block: 'default'})},				
+		'works': function(e) {scroll(e, {direction: 'x', quantity: 4, target: $('#workSecContainer'), block: 'works'})},		
+	};
+	data.scrollFuncMapping = ['default', 'default', 'default', 'default', 'works', 'default'];
+	module.exports = data;
+});
---
+++ scripts/main.js
@@ -0,0 +1,66 @@
+/**
+ *
+ * Created by mingtao on 15-7-28.
+ */
+define(function(require) {
+	require('jquery');
+	var Data = require('./data');
+	$(document).ready(function() {
+	//为所有的透明图绑定事件，鼠标悬停：彩图出现，该图的形状变为彩图形状
+		(function () {
+			$('.hoverImgHandle').bind('mouseover', function() {
+				var imgID = this.id.replace(/Handle/, '');
+				$('#' + imgID).css('display', 'block');
+
+			});
+			$('.hoverImg').bind('mouseout', function(){
+				$(this).css('display','none');
+			})
+		})();
+	});
+
+	//为处理一个bug,暂不知道出现的原因.由于只有在调整页面才会出现该bug,加载时不会,所以放到最后
+	/*window.addEventListener('resize', function(){
+		var html = document.getElementsByTagName('html')[0];
+		if(html.offsetWidth/html.offsetHeight < 16/9) {
+			var width = html.offsetHeight *16/9 +'px';
+			document.getElementById('hoverImgContainer').style.width = width;
+			document.getElementById('bgImg').style.width = width;
+		}
+		else {
+			document.getElementById('hoverImgContainer').style.width = '100%';
+			document.getElementById('bgImg').style.width = '100%';
+		}
+	});*/
+	$('#menu').bind('click', function() {
+		if($(this).attr('status') && $(this).attr('status') == 'unfolded') {
+			$(this).css('transform', 'rotate(0deg)');
+			$(this).attr('status', 'folded');
+			$('#nav').css('width', '32px');
+		} else {
+			$(this).css('transform', 'rotate(1800deg)');
+			$(this).attr('status', 'unfolded');
+			$('#nav').css('width', '525px');
+		}
+	});
+	$(window).bind('mousewheel DOMMouseScroll keydown', function(e) {
+		var funcName = Data.scrollFuncMapping[Data.block['default']];
+		Data.scrollFunc[funcName](e);
+	});
+	
+	//绑定渐变结束事件,重新激活绑定的事件
+	$('#main, #workSecContainer').bind('transitionend webkitTransitionEnd mozTransitionEnd oTransitionEnd', function(e) {
+		Data.changing = false;
+	});
+	$('#cover').bind('transitionend webkitTransitionEnd mozTransitionEnd oTransitionEnd', function(e) {
+		$(this).attr('style', 'display:none');
+	});
+
+	//测试用
+	$(document).ready(function() {
+		$('#cover').css('opacity',0);
+		$('#load2Container').css('top','29%');
+		clearInterval(window.wave);
+		Data.isActive = true;
+	});	
+});
